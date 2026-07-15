cfc601e1d5f25fa1ba78b804e92a89479e52165e
hxx-dev <red05077@gmail.com>
2026-07-15 19:27:07 +0900

    :tada:Start: 프로젝트 시작

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Diary from "./pages/Diary";
+import NotFound from "./pages/NotFound";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/new" element={<New />} />
+      <Route path="/diary" element={<Diary />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+}
+
+export default App;
