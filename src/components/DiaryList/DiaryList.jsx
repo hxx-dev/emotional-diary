3ed9f98ccd272573de6997329ccb0eec12ad90ca
hxx-dev <hxxchangdev@gmail.com>
2026-07-18 20:54:30 +0900

    :sparklse:Feat: 정렬 기능 코드 추가 및 새 일기 쓰기 버튼 navigate 기능 추가

--- src/components/DiaryList/DiaryList.jsx
+++ src/components/DiaryList/DiaryList.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 import Button from "../common/Button";
 import DiaryItem from "../DiaryItem/DiaryItem";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
 
 const MenuBar = styled.div`
   margin: 20px 0px;
@@ -21,18 +23,44 @@ const NewButton = styled(Button)`
   flex: 1;
 `;
 
-function DiaryList() {
+function DiaryList({ data }) {
+  const nav = useNavigate();
+  // 정렬 타입 상태 관리
+  const [sortType, setSortType] = useState("latest");
+  // 정렬 onChange 핸들러
+  const onChangeSortType = (e) => {
+    setSortType(e.target.value);
+  };
+  // 정렬 함수
+  const getSortedData = () => {
+    return data.toSorted((a, b) => {
+      if (sortType === "oldest") {
+        return a.createDate - b.createDate;
+      } else {
+        return b.createDate - a.createDate;
+      }
+    });
+  };
+  // 정렬 함수 저장
+  const SortedData = getSortedData();
+
   return (
     <div>
       <MenuBar>
-        <SelectBar>
+        <SelectBar onChange={onChangeSortType}>
           <option value={"latest"}>최신 순</option>
           <option value={"oldest"}>오래된 순</option>
         </SelectBar>
-        <NewButton text={"새 일기 쓰기"} type={"POSITIVE"} />
+        <NewButton
+          onClick={() => nav("/new")}
+          text={"새 일기 쓰기"}
+          type={"POSITIVE"}
+        />
       </MenuBar>
       <div>
-        <DiaryItem />
+        {SortedData.map((item) => ( //정렬된 데이터로 매핑
+          <DiaryItem key={item.id} {...item} />
+        ))}
       </div>
     </div>
   );
