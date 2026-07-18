7bc90db5d8e5a98b73f393ae133eb4ddb545904c
hxx-dev <hxxchangdev@gmail.com>
2026-07-18 23:29:55 +0900

    New 페이지 UI 구현

--- src/pages/New.jsx
+++ src/pages/New.jsx
@@ -1,5 +1,18 @@
+import styled from "styled-components";
+import Header from "../components/Header/Header";
+import Button from "../components/common/Button";
+import Editor from "../components/Editor/Editor";
+
 function New() {
-  return <div>New</div>;
+  return (
+    <div>
+      <Header
+        title={"새 일기 쓰기"}
+        leftBtn={<Button text={"< 뒤로 가기"} />}
+      />
+      <Editor />
+    </div>
+  );
 }
 
 export default New;
