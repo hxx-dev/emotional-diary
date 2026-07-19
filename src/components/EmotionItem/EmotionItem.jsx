6f40fe88135cb978db5431ab6e50023b73be5ad7
hxx-dev <hxxchangdev@gmail.com>
2026-07-19 15:03:37 +0900

    :recycle:Refactor: 분리된 상수 파일 import

--- src/components/EmotionItem/EmotionItem.jsx
+++ src/components/EmotionItem/EmotionItem.jsx
@@ -1,13 +1,6 @@
 import styled from "styled-components";
 import getEmotionImage from "../../util/get-emotion-image";
-
-const COLORS = {
-  1: "rgb(100, 201, 100)",
-  2: "rgb(157, 215, 114)",
-  3: "rgb(253, 206, 23)",
-  4: "rgb(253, 132, 70)",
-  5: "rgb(253, 86, 95)",
-};
+import { COLORS } from "../../util/constants";
 
 const Container = styled.div`
   background-color: ${({ $isSelected, $emotionId }) => ($isSelected ? COLORS[$emotionId] : "rgb(236,236,236)")};
